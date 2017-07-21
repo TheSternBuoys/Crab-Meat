8878a0e35716aa59f89c0a1b3f6ce18e69670b6b
Transformer\Sean <1004005@student.sae.edu.au>
2017-07-21 22:45:37 +1000

    Script - LoadLevel (Added support for multiple layers)

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -4,46 +4,90 @@ using UnityEngine;
 using System.IO;
 
 public class LoadLevel : MonoBehaviour {
-
 	public GameObject wall;
 	public GameObject player;
-	public string path;
+	public GameObject sandGround;
+	public GameObject reefGround;
+	public GameObject atlantisGround;
+	public int level;
+	public int layer;
+	public string levelString;
+	public string layerString;
+	public float objectSize;
+
+	GameObject currentObject;
 	string baseText;
 	string[] lines;
 	string[] lineData;
+	float xChange;
+	float zChange;
+
 
 	// Use this for initialization
 	void Start () {
-
-		//Reads in the CSV and splits the lines into seperate values
-		baseText = File.ReadAllText (path);
-		lines = baseText.Split("\n"[0]);
+		levelString = level.ToString (levelString);
 		instantiateObjects ();
 	}
 
 	//Spawns gameObjects based on the string
 	public void instantiateObjects()
 	{
-		//1st for loop is for which line of text too use
-		for (int y = 0; y < lines.Length; y++) 
+		for(layer = 1; layer < 3; layer++)
 		{
-			//trims the current line into seperate values
-			lineData = (lines [y].Trim ()).Split ("," [0]);
+			//Reads in the CSV and splits the lines into seperate values
+			layerString = layer.ToString (layerString);
+			baseText = File.ReadAllText ("Assets/CsvFiles/level" + levelString + "-" + layerString + ".csv");
+			layerString = "";
+			lines = baseText.Split("\n"[0]);
+			zChange = 0;
 
-			//2nd Nested for loop reads each value in the line selected in the 1st for loop
-			for (int x = 0; x < lineData.Length; x++) 
+			//1st for loop is for which line of text too use
+			for (int z = 0; z < lines.Length; z++) 
 			{
-				if (lineData [x] == "#") 
+				//trims the current line into seperate values
+				lineData = (lines [z].Trim ()).Split ("," [0]);
+				xChange = 0;
+				zChange += objectSize;
+				//2nd Nested for loop reads each value in the line selected in the 1st for loop
+				for (int x = 0; x < lineData.Length; x++) 
 				{
-					Instantiate (wall, transform.position = (new Vector3(Mathf.Round(x),Mathf.Round(y),transform.position.z)), Quaternion.identity);
-				} 
-				if (lineData [x] == "P") 
-				{
-					Instantiate (player, transform.position = (new Vector3(Mathf.Round(x),Mathf.Round(y),transform.position.z)), Quaternion.identity);
-				} 
-				else 
-				{
-					Debug.Log ("nothing");
+					if (layer == 1) {
+						switch (lineData [x]) 
+						{
+						case "SG":
+							currentObject = sandGround;
+							break;
+						case "AG":
+							currentObject = reefGround;
+							break;
+						case "RG":
+							currentObject = atlantisGround;
+							break;
+						default:
+							currentObject = null;
+							break;
+						}
+						if(currentObject != null)
+							Instantiate (currentObject, new Vector3 (xChange, transform.position.y, zChange), Quaternion.identity);
+					}
+
+					if (layer == 2) {
+						switch (lineData [x]) 
+						{
+						case "#":
+							currentObject = wall;
+							break;
+						case "P":
+							currentObject = player;
+							break;
+						default:
+							currentObject = null;
+							break;
+						}
+						if(currentObject != null)
+							Instantiate (currentObject, new Vector3 (xChange, transform.position.y + objectSize, zChange), Quaternion.identity);
+					}
+					xChange += objectSize;
 				}
 			}
 		}
