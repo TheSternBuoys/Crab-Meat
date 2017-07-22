23d48e9d0db9092fa048c3bd39fb313c998ea977
Transformer\Sean <1004005@student.sae.edu.au>
2017-07-22 10:21:18 +1000

    Script - LoadLevel (Added comments and changed some useless public variables) - CameraToCanvas (new script which adds a camera based on a tagName to the canvas Renderer)

---
+++ Assets/Scripts/CameraToCanvas.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraToCanvas : MonoBehaviour {
+
+	[Tooltip("Tag name of the camera to add.")]
+	public string cameraTagName;
+
+	private Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		SetCamera ();
+	}
+
+	void SetCamera()
+	{
+		//Grabs the current canvas
+		Canvas canvas = gameObject.GetComponent<Canvas> ();
+		//Adds the camera based on the string of cameraTagName to cam variable
+		cam = GameObject.FindGameObjectWithTag (cameraTagName).GetComponent<Camera>();
+		if (canvas != null)
+		{
+			canvas.worldCamera = cam;
+		}
+	}
+}
--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 using System.IO;
 
 public class LoadLevel : MonoBehaviour {
+	//GameObjects to add to inspector/layers
 	public GameObject wall;
 	public GameObject player;
 	public GameObject sandGround;
 	public GameObject reefGround;
 	public GameObject atlantisGround;
+
 	public int level;
-	public int layer;
-	public string levelString;
-	public string layerString;
 	public float objectSize;
 
 	GameObject currentObject;
@@ -21,6 +20,9 @@ public class LoadLevel : MonoBehaviour {
 	string[] lineData;
 	float xChange;
 	float zChange;
+	int layer;
+	string levelString;
+	string layerString;
 
 
 	// Use this for initialization
@@ -39,19 +41,28 @@ public class LoadLevel : MonoBehaviour {
 			baseText = File.ReadAllText ("Assets/CsvFiles/level" + levelString + "-" + layerString + ".csv");
 			layerString = "";
 			lines = baseText.Split("\n"[0]);
+
+			//reset z axis
 			zChange = 0;
 
-			//1st for loop is for which line of text too use
+			//1st Nested for loop is for which line of text too use
 			for (int z = 0; z < lines.Length; z++) 
 			{
 				//trims the current line into seperate values
 				lineData = (lines [z].Trim ()).Split ("," [0]);
+
+				//Reseting x for new line and moving z axis to apply the next line
 				xChange = 0;
 				zChange += objectSize;
 				//2nd Nested for loop reads each value in the line selected in the 1st for loop
 				for (int x = 0; x < lineData.Length; x++) 
 				{
-					if (layer == 1) {
+					//Follow implementation below to add new objects
+					//Layer 1 is all of the layer 1 objects (floors mainly)
+					//Only worry about adding in a new case, creating a public variable at the start of this script
+					//Then adding the required object to the public gameObject variable in the inspector
+					if (layer == 1) 
+					{
 						switch (lineData [x]) 
 						{
 						case "SG":
@@ -71,7 +82,9 @@ public class LoadLevel : MonoBehaviour {
 							Instantiate (currentObject, new Vector3 (xChange, transform.position.y, zChange), Quaternion.identity);
 					}
 
-					if (layer == 2) {
+					//Layer 2 is for the walls,player,enemies etc..
+					if (layer == 2) 
+					{
 						switch (lineData [x]) 
 						{
 						case "#":
@@ -87,6 +100,7 @@ public class LoadLevel : MonoBehaviour {
 						if(currentObject != null)
 							Instantiate (currentObject, new Vector3 (xChange, transform.position.y + objectSize, zChange), Quaternion.identity);
 					}
+					//move x axis to the next instantiate location
 					xChange += objectSize;
 				}
 			}
