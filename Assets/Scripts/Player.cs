806d4b66487951e162d5f1912ffc5848a3f17bd2
Transformer\Sean <1004005@student.sae.edu.au>
2017-07-23 23:23:44 +1000

    Script - Player (player movement and collisions)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	public float speed;
+	public float distanceToMove;
+	public float moveSpeed;
+	public float distanceCheck;
+	public LayerMask mask;
+
+	private bool moveToEnd = false;
+	private Vector3 endPosition;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		endPosition = transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () 
+	{
+		if (moveToEnd == true) 
+		{
+			transform.position = Vector3.MoveTowards (transform.position, endPosition, moveSpeed * Time.deltaTime);
+		}
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.UpArrow)) 
+		{
+			if (Physics.Raycast (transform.position, transform.forward, distanceCheck, mask) == false) 
+			{
+				endPosition = new Vector3 (endPosition.x, endPosition.y, endPosition.z + distanceToMove);
+				moveToEnd = true;
+			}
+		}
+		else if (Input.GetKeyDown (KeyCode.DownArrow)) 
+		{
+			if (Physics.Raycast (transform.position, transform.forward * -1f, distanceCheck, mask) == false) 
+			{
+				endPosition = new Vector3 (endPosition.x, endPosition.y, endPosition.z - distanceToMove);
+				moveToEnd = true;
+			}
+		}
+		else if (Input.GetKeyDown (KeyCode.RightArrow)) 
+		{
+			if (Physics.Raycast (transform.position, transform.right, distanceCheck, mask) == false) 
+			{
+				endPosition = new Vector3 (endPosition.x + distanceToMove, endPosition.y, endPosition.z);
+				moveToEnd = true;
+			}
+
+		}
+		else if (Input.GetKeyDown (KeyCode.LeftArrow)) 
+		{
+			if (Physics.Raycast (transform.position, transform.right * -1f, distanceCheck, mask) == false) 
+			{
+				endPosition = new Vector3 (endPosition.x - distanceToMove, endPosition.y, endPosition.z);
+				moveToEnd = true;
+			}
+		}
 	}
 }
