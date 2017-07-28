a6ccf8480875b2b8fa9e48d51aed8b1e2512df6e
TIGER-PC\Tiger <tiger.collins@outlook.com>
2017-07-28 20:03:59 +1000

    Added a pause menu and freezing time when paused. Added content to the credits screen. Every scene now has a placeholder. Rebaked lighting. Adjusted every scenes canvas to be 1920x1080

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -4,42 +4,65 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour {
 
+    public GameObject Pause;
 
     public void MainMenu()
     {
         Application.LoadLevel("Main Menu");
-    }
-
-    public void Options()
-    {
-        Application.LoadLevel("Options");
+        Time.timeScale = 1;
     }
 
     public void Credits()
     {
         Application.LoadLevel("Credits");
+        Time.timeScale = 1;
     }
 
     public void QuitGame()
     {
         Application.Quit();
         print("Quit");
+        Time.timeScale = 1;
     }
 
     public void LoadLevel1()
     {
         Application.LoadLevel("Level 1");
+        Time.timeScale = 1;
     }
 
     public void LoadIntroCutscene()
     {
         Application.LoadLevel("Intro");
+        Time.timeScale = 1;
     }
 
     public void Retry()
     {
         Application.LoadLevel(Application.loadedLevel);
+        Time.timeScale = 1;
+    }
+    
+    public void UnPauseGame()
+    {
+             if (Pause.activeInHierarchy == true)
+        {
+            Time.timeScale = 1;
+            Pause.SetActive(false);
+        }
     }
 
-    
+    public void PauseGame()
+    {
+        if (Pause.activeInHierarchy == false)
+        {
+            Time.timeScale = 0;
+            Pause.SetActive(true);
+            print("boi");
+        }
+
+    }
+
+
+
 }
