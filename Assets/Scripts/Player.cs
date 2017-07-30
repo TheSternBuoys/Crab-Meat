dba6f8557ec98d6e2da26be347a7ceadd86633f6
TIGER-PC\Tiger <tiger.collins@outlook.com>
2017-07-31 02:08:11 +1000

    Swipe controls, moved filter

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,8 @@ public class Player : MonoBehaviour {
 	private bool moveToEnd = false;
 	private Vector3 endPosition;
 
+    private Vector2 touchOrigin = -Vector2.one;
+
 	Ray ray;
 	RaycastHit hit;
 
@@ -33,39 +35,88 @@ public class Player : MonoBehaviour {
 
 	void Update()
 	{
-		if (Input.GetKeyDown (KeyCode.UpArrow)) 
+        int horizontal = 0;     //Used to store the horizontal move direction.
+        int vertical = 0;       //Used to store the vertical move direction.
+
+#if UNITY_STANDALONE || UNITY_WEBPLAYER
+        horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
+        vertical = (int) (Input.GetAxisRaw ("Verticle"));
+
+        if(horizontal != 0)
+        {
+            vertical = 0;
+        }
+
+#else
+        if (Input.touchCount > 0)
+        {
+            Touch myTouch = Input.touches[0];
+
+            if (myTouch.phase == TouchPhase.Began)
+            {
+                touchOrigin = myTouch.position;
+            }
+
+            else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+            {
+                Vector2 touchEnd = myTouch.position;
+                float x = touchEnd.x - touchOrigin.x;
+                float y = touchEnd.y - touchOrigin.y;
+                touchOrigin.x = -1;
+                if (Mathf.Abs(x) > Mathf.Abs(y))
+                {
+                    horizontal = x > 0 ? 1 : -1;
+                }
+
+                else
+                {
+                    vertical = y > 0 ? 1 : -1;
+                }
+            }
+        }
+
+#endif
+
+        if (vertical == 1) 
 		{
 			if (Physics.Raycast (transform.position, transform.forward, distance, mask) == false) 
 			{
 				endPosition = new Vector3 (endPosition.x, endPosition.y, endPosition.z + distanceToMove);
 				moveToEnd = true;
 			}
+            print("Input");
+            print(vertical);
 		}
-		else if (Input.GetKeyDown (KeyCode.DownArrow)) 
+		else if (vertical == -1) 
 		{
 			if (Physics.Raycast (transform.position, transform.forward * -1f, distance, mask) == false) 
 			{
 				endPosition = new Vector3 (endPosition.x, endPosition.y, endPosition.z - distanceToMove);
 				moveToEnd = true;
 			}
-		}
-		else if (Input.GetKeyDown (KeyCode.RightArrow)) 
+            print("Input");
+            print(vertical);
+        }
+		else if (horizontal == 1) 
 		{
 			if (Physics.Raycast (transform.position, transform.right, distance, mask) == false) 
 			{
 				endPosition = new Vector3 (endPosition.x + distanceToMove, endPosition.y, endPosition.z);
 				moveToEnd = true;
 			}
-
-		}
-		else if (Input.GetKeyDown (KeyCode.LeftArrow)) 
+            print("Input");
+            print(horizontal);
+        }
+		else if (horizontal == -1) 
 		{
 			if (Physics.Raycast (transform.position, transform.right * -1f, distance, mask) == false) 
 			{
 				endPosition = new Vector3 (endPosition.x - distanceToMove, endPosition.y, endPosition.z);
 				moveToEnd = true;
 			}
-		}
+            print("Input");
+            print(horizontal);
+        }
 	}
 
 	/*if (Physics.Raycast (ray, out hit, distance)) 
